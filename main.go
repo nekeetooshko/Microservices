d37ec1b5d1aaec1e4a89612c377983a039690594
Nikita Filichev <richardplantagenetlh@gmail.com>
2024-12-25 19:32:54 +0300

    Add strange graceful shutdown realization. Its not mine, it'a author's

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"BuildingMicroservicesWithGo/handlers"
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"time"
 )
 
@@ -12,22 +14,38 @@ func main() {
 
 	log.Println("\033[34;2mThe server is running!\033[0m")
 
-	l := log.New(os.Stdout, "product-api: ", log.LstdFlags) // 1-куда выводим, 2-префикс, 3-настройки флагов
+	l := log.New(os.Stdout, "product-api: ", log.LstdFlags)
 	helloHandler := handlers.NewHello(l)
 	goodbyeHandler := handlers.NewGoodbye(l)
 
-	sm := http.NewServeMux() // Новый роутер
+	sm := http.NewServeMux()
 	sm.Handle("/h", helloHandler)
 	sm.Handle("/g", goodbyeHandler)
 
-	// Перепишем наш сервер
 	server := &http.Server{
 		Addr:         ":9090",
 		Handler:      sm,
-		IdleTimeout:  120 * time.Second, // макс. время, пока коннект с клиентом активен
-		ReadTimeout:  1 * time.Second,   // макс. вр. ожидания запроса на чтение от клиента
-		WriteTimeout: 1 * time.Second,   // макс. вр. ожидания запроса на запись от клиента
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
 	}
 
-	server.ListenAndServe()
+	go func() { // Если без горутины - залочимся
+		err := server.ListenAndServe()
+		if err != nil {
+			l.Fatalf("Error with server starting: %#v", err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal, 2)
+	signal.Notify(sigChan, os.Interrupt, os.Kill)
+
+	sig := <-sigChan
+	l.Printf("\033[31mThe interrupt command received\033[0m: %v\n", sig)
+
+	// Добавим закрытие сервака (аналог GS)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	server.Shutdown(ctx)
 }
