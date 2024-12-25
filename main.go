1b11c4897aacec9ef1345b60bf3bda7d25d21577
Nikita Filichev <richardplantagenetlh@gmail.com>
2024-12-25 19:18:17 +0300

    Rewrite the server structure:

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
@@ -12,7 +13,6 @@ func main() {
 	log.Println("\033[34;2mThe server is running!\033[0m")
 
 	l := log.New(os.Stdout, "product-api: ", log.LstdFlags) // 1-куда выводим, 2-префикс, 3-настройки флагов
-
 	helloHandler := handlers.NewHello(l)
 	goodbyeHandler := handlers.NewGoodbye(l)
 
@@ -20,7 +20,14 @@ func main() {
 	sm.Handle("/h", helloHandler)
 	sm.Handle("/g", goodbyeHandler)
 
-	if err := http.ListenAndServe(":9090", sm); err != nil {
-		l.Fatalf("Error with server running: %#v\n", err)
-	} // Используем наш ServeMux
+	// Перепишем наш сервер
+	server := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second, // макс. время, пока коннект с клиентом активен
+		ReadTimeout:  1 * time.Second,   // макс. вр. ожидания запроса на чтение от клиента
+		WriteTimeout: 1 * time.Second,   // макс. вр. ожидания запроса на запись от клиента
+	}
+
+	server.ListenAndServe()
 }
