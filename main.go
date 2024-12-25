354e4f575f1f71b8e2a3d667108b57359f864020
Nikita Filichev <richardplantagenetlh@gmail.com>
2024-12-25 17:49:37 +0300

    Add graceful shutdown realization, text highlighting and simple rewrite server structure

--- main.go
+++ main.go
@@ -1,20 +1,42 @@
 package main
 
 import (
+	"context"
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
 
-	log.Println("The server is running!")
+	// Играюсь с цветами вывода (escape-команды). Чо кринж то сразу?
+	log.Println("\033[34mThe server is running!\033[0m")
 
 	// Хэндлеры (регистрируют функции в DefaultServeMux)
 	http.HandleFunc("/h", helloHandler)
 	http.HandleFunc("/g", goodbyeHandler)
 
-	http.ListenAndServe(":9090", nil) // Используем DefaultServeMux
+	server := http.Server{Addr: ":9090"}
+
+	/* Здесь выбрана такая реализция: сервак на горутине, GS на main'e, ведь если бы мы выбрали обратку
+	(сервак на main, GS на горутине), то мы бы просто застопали main функцией ListenAndServe.
+	Таким образом, он не сможет обрабатывать сигналы, пока сервер не завершит работу, что делает невозможным
+	точное реагирование на Ctrl + C до тех пор, пока сервер не завершит свои операции.
+	*/
+
+	go func() {
+
+		err := http.ListenAndServe(server.Addr, nil)
+		if err != nil {
+			log.Fatalf("Error with server running: %#v\n", err)
+		}
+	}()
+
+	gracefulShutdown(&server)
 }
 
 // Приветственный хендлер
@@ -36,4 +58,27 @@ func helloHandler(rw http.ResponseWriter, req *http.Request) {
 func goodbyeHandler(rw http.ResponseWriter, req *http.Request) {
 	log.SetOutput(rw)
 	log.Println("Goodbye from client!")
+}
+
+// Graceful shutdown
+func gracefulShutdown(server *http.Server) {
+
+	signalChan := make(chan os.Signal, 2)
+
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
+	select { // Он тут по факту нахуй не нужен, но меня научили писать GS на нем
+
+	case sig := <-signalChan: // Можно было бы без присваивания, но я вывожу сигнал, так что
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
+		defer cancel()
+
+		if err := server.Shutdown(ctx); err != nil {
+			log.Fatalf("Error while server is shutting down: %#v\n", err)
+		}
+
+		// Здесь стоит escape-команда. Это моя шиза. Все хорошо.
+		log.Printf("\033[31mConnection stop by signal: %v, %#v\n\033[0m", sig, sig)
+	}
 }
