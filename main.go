176388dce8adedf0727830a1d50728a74387ced8
Nikita Filichev <richardplantagenetlh@gmail.com>
2024-12-25 17:25:35 +0300

    Add error checking, reading data from client and sending it back

--- main.go
+++ main.go
@@ -1,25 +1,35 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
 )
 
 func main() {
 
-	http.ListenAndServe(":9090", nil) // Используем DefaultServeMux
 	log.Println("The server is running!")
 
 	// Хэндлеры (регистрируют функции в DefaultServeMux)
 	http.HandleFunc("/h", helloHandler)
 	http.HandleFunc("/g", goodbyeHandler)
 
+	http.ListenAndServe(":9090", nil) // Используем DefaultServeMux
 }
 
 // Приветственный хендлер
 func helloHandler(rw http.ResponseWriter, req *http.Request) {
 	log.SetOutput(rw) // Переустанавливаю базовый вывод инфы
 	log.Println("Hello from client!")
+
+	// Считывам данные из запроса (req.Body - интерфейс io.ReadCloser => нужно что-то, умеющее читать из него)
+	data, err := io.ReadAll(req.Body)
+	if err != nil {
+		http.Error(rw, "Error while reading the body: %e\n", http.StatusBadRequest)
+		return // http.Error не прерывает сервачок
+	}
+	log.Printf("Data: %s\n", string(data)) // Можно было бы через rw.Write, но там нельзя писать в кавычках
+
 }
 
 // Прощальный хендлер
